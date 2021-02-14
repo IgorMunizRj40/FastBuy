062d92a5479db3596659d0d5497900ead5d94052
Igor Muniz <igor.galdinomuniz@gmail.com>
2021-02-14 18:15:25 -0300

    Criação das entidades

---
+++ FastBuy.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,7 @@
+﻿namespace FastBuy.Dominio.Entidades
+{
+    public abstract class Entidade
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ FastBuy.Dominio/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace FastBuy.Dominio.Enumerados
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido = 0,
+        Boleto = 1,
+        CartaoCredito = 2,
+        Deposito = 3,
+        CartaoDebito = 4,
+        Pix = 5
+    }
+}
