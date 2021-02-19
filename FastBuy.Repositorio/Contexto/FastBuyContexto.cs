82330ba1f8c723f327bdc106f750c9c5394f22c5
Igor Muniz <igor.galdinomuniz@gmail.com>
2021-02-18 21:42:42 -0300

    Criação do DbContexto e dos DbSet's

---
+++ FastBuy.Repositorio/Contexto/FastBuyContexto.cs
@@ -0,0 +1,16 @@
+﻿using FastBuy.Dominio.Entidades;
+using FastBuy.Dominio.ObjetoDeValor;
+using Microsoft.EntityFrameworkCore;
+
+namespace FastBuy.Repositorio.Contexto
+{
+    public class FastBuyContexto : DbContext
+    {
+        public DbSet<Usuario>Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+    }
+}
